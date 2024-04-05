8edd434e10f2ec3c9096d406690877200e5db34c
marybilik <165520003+marybilik@users.noreply.github.com>
2024-04-05 14:11:54 +0300

    Update views.py

--- new/new_app/views.py
+++ new/new_app/views.py
@@ -4,9 +4,8 @@ import os
 from django.conf import settings
 import cv2
 import numpy as np
+from .models import RequestHistory
 
-import cv2
-import numpy as np
 
 def generate_video(text, output_path):
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -14,43 +13,44 @@ def generate_video(text, output_path):
     font_color = (255, 255, 255)
     font_thickness = 2
 
-    text_width, _ = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
-
-    frame_width = 640
-    frame_height = 480
+    frame_width = 100
+    frame_height = 100
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_path, fourcc, 20.0, (frame_width, frame_height))
+    out = cv2.VideoWriter(output_path, fourcc, 30, (frame_width, frame_height))
 
     text_x = frame_width
 
-    while text_x > -text_width:
+    while text_x > -len(text) * font_scale:
         frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
 
         cv2.putText(frame, text, (text_x, 50), font, font_scale, font_color, font_thickness)
 
         out.write(frame)
 
-        text_x -= 5  # Изменяем положение текста по горизонтали на каждом кадре
+        text_x -= 1  # Изменяем положение текста по горизонтали на каждом кадре
 
     out.release()
 
-text = "Привет, мир!"
-output_path = "output_video.avi"
-generate_video(text, output_path)
+    history_entry = RequestHistory.objects.create(text=text)
+    history_entry.save()
+
 
 
 
 def download_video(request):
+    
     if request.method == 'POST':
         text = request.POST.get('text', '')  # Получаем текст из формы
 
         video_name = 'running_text.avi'
         generate_video(text, video_name)
 
+        
+
         # Отправляем видео для скачивания
         video_path = os.path.join(settings.BASE_DIR, video_name)
         with open(video_path, 'rb') as video_file:
-            response = HttpResponse(video_file, content_type='video/mp4')
+            response = HttpResponse(video_file, content_type='video/avi')
             response['Content-Disposition'] = f'attachment; filename="{video_name}"'
             return response
 
