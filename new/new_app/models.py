825d215b59a1a66179fe2e637de248531231c73e
marybilik <165520003+marybilik@users.noreply.github.com>
2024-04-05 14:12:23 +0300

    Update models.py

--- new/new_app/models.py
+++ new/new_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class RequestHistory(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
