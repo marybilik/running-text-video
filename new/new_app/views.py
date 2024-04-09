e8b9b4ca3dc92d63e804edb5f5ac303e9151c1d3
marybilik <165520003+marybilik@users.noreply.github.com>
2024-04-09 17:49:14 +0300

    Update views.py

--- new/new_app/views.py
+++ new/new_app/views.py
@@ -8,7 +8,7 @@ from .models import RequestHistory
 
 
 def generate_video(text, output_path):
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1
     font_color = (255, 255, 255)
     font_thickness = 2
@@ -16,18 +16,23 @@ def generate_video(text, output_path):
     frame_width = 100
     frame_height = 100
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    
+    text_length = cv2.getTextSize(text, font, font_scale, font_thickness)[0][0]
+    scroll_speed = text_length / (3 * 30)  # Вычисляем скорость прокрутки текста для показа за 3 секунды
+
     out = cv2.VideoWriter(output_path, fourcc, 30, (frame_width, frame_height))
 
     text_x = frame_width
 
-    while text_x > -len(text) * font_scale:
+    while text_x > -text_length:
         frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
 
-        cv2.putText(frame, text, (text_x, 50), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, text, (int(text_x), 50), font, font_scale, font_color, font_thickness)
+
 
         out.write(frame)
 
-        text_x -= 1  # Изменяем положение текста по горизонтали на каждом кадре
+        text_x -= scroll_speed
 
     out.release()
 
